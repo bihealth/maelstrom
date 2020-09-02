a33cf0f2d134bde5b76e784541b881bbe4e84fe8
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-09-02 08:20:11 +0200

    :zap: Completing maelstrom-snakemake

--- maelstrom/snakemake/__init__.py
+++ maelstrom/snakemake/__init__.py
@@ -1,12 +1,25 @@
 """maelstrom-snakemake -- call Snakemake with the Maelstrom Snakemake file."""
 
 import pathlib
+import shlex
 import subprocess
 import sys
 import typing
 
 #: Path to Snakefile.
 PATH_SNAKEFILE = pathlib.Path(__file__).parent / "Snakefile"
+
+
+def which(name: str) -> typing.Optional[str]:
+    """Calls ``which`` and returns the path to the executable or None if there is none."""
+    try:
+        return (
+            subprocess.check_output(["which", shlex.quote(name)], stderr=subprocess.PIPE)
+            .decode("utf-8")
+            .strip()
+        )
+    except subprocess.CalledProcessError:
+        return None
 
 
 def main(args=None) -> int:
