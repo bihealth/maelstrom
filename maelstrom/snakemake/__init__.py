58463c297ab607f66eab3f98822bfd7c5eed5845
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2020-09-02 07:48:12 +0200

    :zap: Adding initial code.

---
+++ maelstrom/snakemake/__init__.py
@@ -0,0 +1,12 @@
+"""maelstrom-snakemake -- call Snakemake with the Maelstrom Snakemake file."""
+
+import subprocess
+import sys
+
+
+def main(args=None):
+    print(args)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
